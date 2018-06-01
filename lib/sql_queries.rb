ef198daf537c9d5604d237c556a9180460bbd660
cindych419@gmail.com <cindych419@gmail.com>
2018-06-01 19:55:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,10 +43,11 @@ ORDER BY(SUM(pledges.amount)) ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
 FROM projects
 LEFT JOIN pledges
 ON projects.id = pledges.project_id
+GROUP BY 
 HAVING projects.category = 'music';"
 end
 
