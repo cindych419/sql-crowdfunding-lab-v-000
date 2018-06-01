521459dc1deef8e87733fd5a30aa05dfeec5ce2e
cindych419@gmail.com <cindych419@gmail.com>
2018-06-01 19:02:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ LEFT JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY(projects.title)
 ORDER BY(projects.title) ASC
-HAVING SUM(pledges.amount) > projects.funding_goal;"
+WHERE SUM(pledges.amount) > projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
